2b5dc8d29abce45c5845ef72cf02413de4d3493f
Ginikachukw <faithgeorge202@gmail.com>
2022-08-19 16:37:49 +0000

    first

--- Truffle/truffle-config.js
+++ Truffle/truffle-config.js
@@ -1,37 +1,30 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');ihk
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
-  networks: {
-    develop: {
-      host: "127.0.0.1",
-      port: 7545,
-      chainId: 1337,
-      network_id: 1337,
+  // something huge
+  networks: { 
+
+    testnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
+   
   },
-  //
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  //   enabled: true,
-  //   host: "127.0.0.1",
-  //   adapter: {
-  //     name: "sqlite",
-  //     settings: {
-  //       directory: ".db",
-  //     },
-  //   },
-  // },
-};
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.4", // A version or constraint - Ex. "^0.5.0"
+    }
+  }
+}
